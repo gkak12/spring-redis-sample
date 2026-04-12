1897b56ac39fdbafc8ea9dcd68ef069011306757
gkak12 <hyunjun5681@kakao.com>
2026-04-12 19:08:16 +0900

    문자열/객체 모두 처리 가능하도록 RedisConfig 수정

--- src/main/kotlin/com/spring/redis/sample/config/RedisConfig.kt
+++ src/main/kotlin/com/spring/redis/sample/config/RedisConfig.kt
@@ -1,22 +1,33 @@
 package com.spring.redis.sample.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig {
 
+    /**
+     * String/객체 모두 처리하는 단일 RedisTemplate
+     * - 키: StringRedisSerializer (사람이 읽기 쉬운 형태)
+     * - 값: Jackson2JsonRedisSerializer (@class 없는 순수 JSON)
+     */
     @Bean
-    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
-        val template = RedisTemplate<String, String>()
+    fun redisTemplate(
+        connectionFactory: RedisConnectionFactory,
+        objectMapper: ObjectMapper
+    ): RedisTemplate<String, Any> {
+        val serializer = Jackson2JsonRedisSerializer(objectMapper, Any::class.java)
+        val template = RedisTemplate<String, Any>()
         template.connectionFactory = connectionFactory
         template.keySerializer = StringRedisSerializer()
-        template.valueSerializer = StringRedisSerializer()
+        template.valueSerializer = serializer
         template.hashKeySerializer = StringRedisSerializer()
-        template.hashValueSerializer = StringRedisSerializer()
+        template.hashValueSerializer = serializer
         return template
     }
 }
